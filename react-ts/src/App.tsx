b73148f456c86cd5014a9b54687157ba57191967
Adrian M.A <adrian@neurocadi.es>
2020-07-27 17:41:34 +0200

    conditioning view elements

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -4,22 +4,40 @@ import { fetchQuizQuestions } from './API';
 // Components
 import QuestionCard from './components/QuestionCard';
 // Types
-import { Difficulty } from './API'
+import {QuestionState, Difficulty } from './API'
 
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 const App = () => {
 
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswer, setUserAnswers] = useState([]);
+  const [userAnswer, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY ))
-
   const startTrivia = async () =>{
+
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
 
   }
 
@@ -34,11 +52,16 @@ const App = () => {
   return (
     <div className="App">
       <h1>REACT-TS Quiz</h1>
-      <button className="start" onClick={startTrivia}>
+      {gameOver || setUserAnswers.length === TOTAL_QUESTIONS
+        ?
+        <button className="start" onClick={startTrivia}>
         Start
-      </button>
-      <p className="score">Score:</p>
-      <p> Loading Questions...</p>
+        </button>
+        :
+        null
+      }
+      {!gameOver ? <p className="score">Score:</p> : null}
+      {loading ? <p> Loading Questions...</p> : null}
       {/* <QuestionCard
         questionNr={number +1}
         totalQuestions={TOTAL_QUESTIONS}
